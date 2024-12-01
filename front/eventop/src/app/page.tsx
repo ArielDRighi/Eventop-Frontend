041552184a5ad74d9f4c5766b9f42f0fb42f51e3
Mateo Lampasona <mateolampasona7@gmail.com>
2024-11-30 22:13:03 -0300

    update page
    saque el boton de registrar con google de page

--- front/eventop/src/app/page.tsx
+++ front/eventop/src/app/page.tsx
@@ -6,24 +6,8 @@ import Blog from "@/components/Blog";
 import BlogTwo from "@/components/BlogTwo";
 import EventsPassed from "@/components/EventsPassed";
 import Opinions from "@/components/Opinions";
-import { useEffect } from "react";
 
 const Home = () => {
-  const handleGoogleLogin = () => {
-    window.location.href = "http://localhost:3000/auth/google/login";
-  };
-  useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const token = urlParams.get("token");
-    console.log("token", token);
-
-    if (token) {
-      localStorage.setItem("jwtToken", token);
-
-      window.location.href = "http://localhost:3000";
-    }
-  }, []);
-
   return (
     <div className="bg-gray-900">
       <SectionOne />
@@ -32,9 +16,6 @@ const Home = () => {
       <BlogTwo />
       <Opinions />
       <Blog />
-      <div className="App">
-        <button onClick={handleGoogleLogin}>Login With Google</button>
-      </div>
     </div>
   );
 };
